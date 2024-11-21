e3745546c2e50020db4206ff01414dda8c2be6ae
Mohit Malik <malikmohit332@gmail.com>
2024-11-22 01:09:50 +0530

    Added user signup and login as well as user authentication functionality

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,19 +1,31 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+
+import { Form, Link, useSearchParams, useActionData, useNavigation } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  const data = useActionData();
+  const navigation = useNavigation(); //this hook gives us a nevigation object.
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const [searchParams, setSearchParams] = useSearchParams(); //these values are given by useSeachParam.
+  //But here we need only searchParam
+
+  const isLogin = searchParams.get('mode') === 'login';
+
+  const isSubmitting = navigation.state === 'submitting';
+  
 
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        {data && data.errors && <ul> 
+            {Object.values(data.errors).map(
+              err => <li key={err}>{err}</li>)
+            }
+          </ul>
+        }
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -23,10 +35,10 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
-          <button>Save</button>
+          </Link>
+          <button disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : 'Save'}</button> 
         </div>
       </Form>
     </>
@@ -34,3 +46,12 @@ function AuthForm() {
 }
 
 export default AuthForm;
+
+
+//React router gives us a hook that makes it easy to get access to the currently set query paramerer.
+// and that hook is " useSearchParams ".
+
+//This useSearchParams hook returns an Array.
+
+//The data.errors provides us an object. and we can get its values by using built in function,
+// 'Object.values'
--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json, redirect } from 'react-router-dom'
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,50 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+
+export async function action ({ request }) {
+  const searchParam = new URL(request.url).searchParams;
+  const mode = searchParam.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup'){
+    throw json({message: 'Unsupported Mode'}, {status: 422});
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(authData),
+  })
+
+  if (response.status === 422 || response.status === 401){
+    return response;
+  }
+
+  if (!response.ok){
+    throw json({ message: 'Could not authenticate user' }, { status: 500 })
+  }
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem('token', token);
+
+  return redirect('/')
+}
+
+
+//The action here will be triggered whenever the used <AuthForm> is submitted, because it is 
+// on the same route as this <AuthForm> is on.
+
+//Now we need to access the data that was submitted in the form (here, Username & password) to
+// authenticate the use. And that we can access by using {request} Object, that's the part of the 
+// package that we get as parameter in this action function, that is executed by react router. 
